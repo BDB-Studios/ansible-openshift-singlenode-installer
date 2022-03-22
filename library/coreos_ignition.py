7e233737a9e17fe370b0eedb59f11a57c6b4ff4a
Brett Minnie <github@bdb-studios.co.uk>
2022-03-22 10:19:14 +0000

    Made library slightly more idempotent

--- library/coreos_ignition.py
+++ library/coreos_ignition.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from ansible.module_utils.basic import *
+from ansible.module_utils.basic import AnsibleModule
 import json
 import ast
 
@@ -27,7 +27,13 @@ options:
       description
         - The JSON block, as a string we wish to add
       required: true
-
+    state:
+      description
+        - Do we add or remove this block
+      choices: [ 'present', 'absent' ]
+      required: false
+author:
+    - Brett Minnie (@brettminnie)
 '''
 EXAMPLES = '''
 # Add a file to our coreos ignition
@@ -42,31 +48,95 @@ EXAMPLES = '''
 class CoreosIgnition(object):
 
     def __init__(self):
-        self.module = AnsibleModule(
-            argument_spec=dict(
-                path=dict(default='/openshift/iso/iso.ign'),
-                section=dict(required=True, type='str'),
-                subsection=dict(required=True, type='str'),
-                content=dict(required=True, type='str')
+        self.arguments = dict(
+            path=dict(
+                default='/openshift/iso/iso.ign'
+            ),
+            section=dict(
+                required=True,
+                type='str'
+            ),
+            subsection=dict(
+                required=True,
+                type='str'
+            ),
+            content=dict(
+                required=True,
+                type='str'
+            ),
+            state=dict(
+                default='present',
+                choices=['absent', 'present']
             )
         )
 
-        self.params = self.module.params
-        self._write_contents()
-        self.module.exit_json(changed=False)
+        self.module = AnsibleModule(
+            argument_spec=self.arguments
+        )
 
-    def _write_contents(self):
+        self.params = self.module.params
+        self.json_data = self.__load_json()
+        self.result = dict(
+            changed=False,
+            original_message='',
+            message=''
+        )
+
+    def run_module(self):
+        if self._is_present():
+            self._change_contents()
+        elif not self._is_present():
+            self._remove_content()
+
+        self.__write_json()
+
+        self.module.exit_json(**self.result)
+
+    def _is_present(self):
+        """
+        Checks to see if our state is present
+        :return: boolean
+        """
+        return str(self.params['state']).lower() == 'present'
+
+    def __load_json(self):
         raw = open(self.params['path'])
         data = json.load(raw)
-        dict = ast.literal_eval(self.params['content'])
-        data[self.params['section']][self.params['subsection']].append(dict)
-        with open(self.params['path'], 'w') as outfile:
-            json.dump(data, outfile)
+        raw.close()
+        return data
 
+    def __write_json(self):
+        with open(self.params['path'], 'w') as outfile:
+            json.dump(self.json_data, outfile)
         outfile.close()
 
-        self.module.exit_json(changed=True, meta={"Result": self.params['content']})
+    def _content_exists(self):
+        try:
+            if self.params['subsection'] in self.json_data[self.params['section']]:
+                return True
+        except KeyError:
+            pass
+        return False
+
+    def _remove_content(self):
+        if self._content_exists():
+            del(self.json_data[self.params['section']][self.params['subsection']])
+
+    def _change_contents(self):
+        content_dictionary = ast.literal_eval(self.params['content'])
+        if not self._content_exists():
+            self.json_data[self.params['section']][self.params['subsection']].append(content_dictionary)
+        else:
+            self.result['original_message'] = str(self.json_data[self.params['section']][self.params['subsection']])
+            self.json_data[self.params['section']][self.params['subsection']].update(content_dictionary)
+        self.result['changed'] = True
+        self.result['message'] = str(content_dictionary)
+
+
+def main():
+    module = CoreosIgnition()
+    module.run_module()
 
 
 if __name__ == '__main__':
-    CoreosIgnition()
+    main()
